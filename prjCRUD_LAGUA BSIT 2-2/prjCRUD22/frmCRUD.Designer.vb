b9f3598c83676ecd383167ca2fba6249787cf858
lagwoaaah <117020157+lagwoaaah@users.noreply.github.com>
2022-10-30 21:36:44 +0800

    Add files via upload

---
+++ prjCRUD_LAGUA BSIT 2-2/prjCRUD22/frmCRUD.Designer.vb
@@ -0,0 +1,370 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCRUD
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.pnlHeader = New System.Windows.Forms.Panel()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.pnlFooter = New System.Windows.Forms.Panel()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.btnEdit = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnNew = New System.Windows.Forms.Button()
+        Me.pnlDataEntry = New System.Windows.Forms.Panel()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtUnitPrice = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtDescription = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtBrand = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.dgv = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Bramd = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.pnlHeader.SuspendLayout()
+        Me.pnlFooter.SuspendLayout()
+        Me.pnlDataEntry.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pnlHeader
+        '
+        Me.pnlHeader.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.pnlHeader.Controls.Add(Me.txtSearch)
+        Me.pnlHeader.Controls.Add(Me.btnSearch)
+        Me.pnlHeader.Controls.Add(Me.Label1)
+        Me.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top
+        Me.pnlHeader.Location = New System.Drawing.Point(0, 0)
+        Me.pnlHeader.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.pnlHeader.Name = "pnlHeader"
+        Me.pnlHeader.Size = New System.Drawing.Size(1163, 64)
+        Me.pnlHeader.TabIndex = 0
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Location = New System.Drawing.Point(809, 32)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(270, 26)
+        Me.txtSearch.TabIndex = 2
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSearch.Location = New System.Drawing.Point(1085, 32)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(75, 26)
+        Me.btnSearch.TabIndex = 1
+        Me.btnSearch.Text = "Searc&h"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(111, 28)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Products"
+        '
+        'pnlFooter
+        '
+        Me.pnlFooter.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.pnlFooter.Controls.Add(Me.btnExit)
+        Me.pnlFooter.Controls.Add(Me.btnDelete)
+        Me.pnlFooter.Controls.Add(Me.btnCancel)
+        Me.pnlFooter.Controls.Add(Me.btnEdit)
+        Me.pnlFooter.Controls.Add(Me.btnSave)
+        Me.pnlFooter.Controls.Add(Me.btnNew)
+        Me.pnlFooter.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.pnlFooter.Location = New System.Drawing.Point(0, 518)
+        Me.pnlFooter.Name = "pnlFooter"
+        Me.pnlFooter.Size = New System.Drawing.Size(1163, 71)
+        Me.pnlFooter.TabIndex = 1
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(1057, 15)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(94, 44)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDelete.Location = New System.Drawing.Point(412, 15)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(94, 44)
+        Me.btnDelete.TabIndex = 4
+        Me.btnDelete.Text = "&Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Enabled = False
+        Me.btnCancel.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCancel.Location = New System.Drawing.Point(312, 15)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(94, 44)
+        Me.btnCancel.TabIndex = 3
+        Me.btnCancel.Text = "&Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'btnEdit
+        '
+        Me.btnEdit.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEdit.Location = New System.Drawing.Point(212, 15)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(94, 44)
+        Me.btnEdit.TabIndex = 2
+        Me.btnEdit.Text = "E&dit"
+        Me.btnEdit.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Enabled = False
+        Me.btnSave.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSave.Location = New System.Drawing.Point(112, 15)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(94, 44)
+        Me.btnSave.TabIndex = 1
+        Me.btnSave.Text = "&Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnNew
+        '
+        Me.btnNew.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNew.Location = New System.Drawing.Point(12, 15)
+        Me.btnNew.Name = "btnNew"
+        Me.btnNew.Size = New System.Drawing.Size(94, 44)
+        Me.btnNew.TabIndex = 0
+        Me.btnNew.Text = "&New"
+        Me.btnNew.UseVisualStyleBackColor = True
+        '
+        'pnlDataEntry
+        '
+        Me.pnlDataEntry.Controls.Add(Me.txtID)
+        Me.pnlDataEntry.Controls.Add(Me.Label5)
+        Me.pnlDataEntry.Controls.Add(Me.txtUnitPrice)
+        Me.pnlDataEntry.Controls.Add(Me.Label4)
+        Me.pnlDataEntry.Controls.Add(Me.txtDescription)
+        Me.pnlDataEntry.Controls.Add(Me.Label3)
+        Me.pnlDataEntry.Controls.Add(Me.txtBrand)
+        Me.pnlDataEntry.Controls.Add(Me.Label2)
+        Me.pnlDataEntry.Dock = System.Windows.Forms.DockStyle.Left
+        Me.pnlDataEntry.Enabled = False
+        Me.pnlDataEntry.Location = New System.Drawing.Point(0, 64)
+        Me.pnlDataEntry.Name = "pnlDataEntry"
+        Me.pnlDataEntry.Size = New System.Drawing.Size(235, 454)
+        Me.pnlDataEntry.TabIndex = 2
+        '
+        'txtID
+        '
+        Me.txtID.Enabled = False
+        Me.txtID.Location = New System.Drawing.Point(12, 42)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(217, 26)
+        Me.txtID.TabIndex = 7
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(8, 19)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(95, 20)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Product ID :"
+        '
+        'txtUnitPrice
+        '
+        Me.txtUnitPrice.Location = New System.Drawing.Point(12, 394)
+        Me.txtUnitPrice.Name = "txtUnitPrice"
+        Me.txtUnitPrice.Size = New System.Drawing.Size(217, 26)
+        Me.txtUnitPrice.TabIndex = 5
+        Me.txtUnitPrice.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(8, 371)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(86, 20)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Unit Price :"
+        '
+        'txtDescription
+        '
+        Me.txtDescription.Location = New System.Drawing.Point(12, 152)
+        Me.txtDescription.Multiline = True
+        Me.txtDescription.Name = "txtDescription"
+        Me.txtDescription.Size = New System.Drawing.Size(217, 210)
+        Me.txtDescription.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(8, 129)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(99, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Description :"
+        '
+        'txtBrand
+        '
+        Me.txtBrand.Location = New System.Drawing.Point(12, 94)
+        Me.txtBrand.Name = "txtBrand"
+        Me.txtBrand.Size = New System.Drawing.Size(217, 26)
+        Me.txtBrand.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(8, 71)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 20)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Brand :"
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.dgv)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel2.Location = New System.Drawing.Point(235, 64)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(928, 454)
+        Me.Panel2.TabIndex = 3
+        '
+        'dgv
+        '
+        Me.dgv.AllowUserToAddRows = False
+        Me.dgv.AllowUserToDeleteRows = False
+        Me.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Bramd, Me.Column2, Me.Column3})
+        Me.dgv.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgv.Location = New System.Drawing.Point(0, 0)
+        Me.dgv.Name = "dgv"
+        Me.dgv.ReadOnly = True
+        Me.dgv.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgv.Size = New System.Drawing.Size(928, 454)
+        Me.dgv.TabIndex = 0
+        '
+        'Column1
+        '
+        Me.Column1.DataPropertyName = "productid"
+        Me.Column1.HeaderText = "ID"
+        Me.Column1.Name = "Column1"
+        Me.Column1.ReadOnly = True
+        '
+        'Bramd
+        '
+        Me.Bramd.DataPropertyName = "brand"
+        Me.Bramd.HeaderText = "Brand"
+        Me.Bramd.Name = "Bramd"
+        Me.Bramd.ReadOnly = True
+        '
+        'Column2
+        '
+        Me.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill
+        Me.Column2.DataPropertyName = "description"
+        Me.Column2.HeaderText = "Description"
+        Me.Column2.Name = "Column2"
+        Me.Column2.ReadOnly = True
+        '
+        'Column3
+        '
+        Me.Column3.DataPropertyName = "unit_price"
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight
+        DataGridViewCellStyle1.Format = "N2"
+        DataGridViewCellStyle1.NullValue = Nothing
+        Me.Column3.DefaultCellStyle = DataGridViewCellStyle1
+        Me.Column3.HeaderText = "Unit Price"
+        Me.Column3.Name = "Column3"
+        Me.Column3.ReadOnly = True
+        '
+        'frmCRUD
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1163, 589)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.pnlDataEntry)
+        Me.Controls.Add(Me.pnlFooter)
+        Me.Controls.Add(Me.pnlHeader)
+        Me.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmCRUD"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.pnlHeader.ResumeLayout(False)
+        Me.pnlHeader.PerformLayout()
+        Me.pnlFooter.ResumeLayout(False)
+        Me.pnlDataEntry.ResumeLayout(False)
+        Me.pnlDataEntry.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents pnlHeader As System.Windows.Forms.Panel
+    Friend WithEvents pnlFooter As System.Windows.Forms.Panel
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
+    Friend WithEvents btnEdit As System.Windows.Forms.Button
+    Friend WithEvents btnSave As System.Windows.Forms.Button
+    Friend WithEvents btnNew As System.Windows.Forms.Button
+    Friend WithEvents pnlDataEntry As System.Windows.Forms.Panel
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents dgv As System.Windows.Forms.DataGridView
+    Friend WithEvents Column1 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Bramd As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtUnitPrice As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtDescription As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtBrand As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtSearch As System.Windows.Forms.TextBox
+    Friend WithEvents btnSearch As System.Windows.Forms.Button
+    Friend WithEvents txtID As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+
+End Class
---
+++ prjCRUD_LAGUA BSIT 2-2/prjCRUD22/frmCRUD.vb
@@ -0,0 +1,191 @@
+﻿Public Class frmCRUD
+    Private Sub frmCRUD_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call loadDGV("SELECT * FROM product", dgv)
+    End Sub
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        Call loadDGV("SELECT * FROM product", dgv, "description", Trim(txtSearch.Text))
+    End Sub
+
+    Private Sub txtSearch_TextChanged(sender As Object, e As EventArgs) Handles txtSearch.TextChanged
+        Call loadDGV("SELECT * FROM product", dgv, "description", Trim(txtSearch.Text))
+    End Sub
+
+    Private Sub toggle(ByVal bool As Boolean)
+        btnNew.Enabled = Not bool
+        btnSave.Enabled = bool
+        btnCancel.Enabled = bool
+        btnEdit.Enabled = Not bool
+        btnDelete.Enabled = Not bool
+        btnExit.Enabled = Not bool
+
+        pnlDataEntry.Enabled = bool
+        dgv.Enabled = Not bool
+        pnlHeader.Enabled = Not bool
+    End Sub
+
+    Private Sub cleaner(ByVal pnl As Panel)
+        For Each obj As Object In pnl.Controls
+            If TypeOf obj Is TextBox Then
+                obj.text = ""
+            End If
+        Next
+    End Sub
+
+    Function checker(ByVal pnl As Panel) As Boolean
+        For Each obj As Object In pnl.Controls
+            If TypeOf obj Is TextBox Then
+                If Len(obj.text) = 0 Then
+                    checker = False
+                    MsgBox("Insufficient Data! Please enter all information.", vbCritical, "SAVE")
+                    obj.focus()
+                    Return False
+                    Exit Function
+                End If
+            End If
+        Next
+        Return True
+    End Function
+
+      Private Sub btnNew_Click(sender As Object, e As EventArgs) Handles btnNew.Click
+        Call cleaner(pnlDataEntry)
+        Call toggle(True)
+        txtID.Text = 0
+        txtBrand.Focus()
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Dim cmd As Odbc.OdbcCommand
+        'Dim strInsert As String = "INSERT INTO product(brand,description,unit_price)VALUES(?,?,?)"
+        'Dim strUpdate As String = "UPDATE product SET brand=?,description=?,unit_price=? WHERE productid=?"
+        Dim sql As String
+        If checker(pnlDataEntry) = True Then
+            Try
+                Call connectDB()
+                If Val(txtID.Text) = 0 Then
+                    sql = "INSERT INTO product(brand,description,unit_price)VALUES(?,?,?)"
+                    cmd = New Odbc.OdbcCommand(sql, con)
+                    cmd.Parameters.AddWithValue("@", Trim(txtBrand.Text))
+                    cmd.Parameters.AddWithValue("@", Trim(txtDescription.Text))
+                    cmd.Parameters.AddWithValue("@", Val(txtUnitPrice.Text))
+                Else
+                    sql = "UPDATE product SET brand=?,description=?,unit_price=? WHERE productid=?"
+                    cmd = New Odbc.OdbcCommand(sql, con)
+                    cmd.Parameters.AddWithValue("@", Trim(txtBrand.Text))
+                    cmd.Parameters.AddWithValue("@", Trim(txtDescription.Text))
+                    cmd.Parameters.AddWithValue("@", Val(txtUnitPrice.Text))
+                    cmd.Parameters.AddWithValue("@", Val(txtID.Text))
+                End If
+                cmd.ExecuteNonQuery()
+                Call cleaner(pnlDataEntry)
+                Call toggle(False)
+                Call loadDGV("SELECT * FROM product", dgv)
+            Catch ex As Exception
+                MsgBox(ex.Message.ToString)
+            Finally
+                con.Close()
+                GC.Collect()
+            End Try
+        End If
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        If MsgBox("Are you sure you want to cancel this operation?", vbYesNo + vbQuestion, "CANCEL") = vbYes Then
+            Call cleaner(pnlDataEntry)
+            Call toggle(False)
+        End If
+    End Sub
+
+    Private Sub dgv_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgv.CellClick
+        Try
+            dgv.Tag = dgv.Item(0, e.RowIndex).Value
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub loadRecord(id As Integer)
+        Dim cmd As Odbc.OdbcCommand
+        Dim da As New Odbc.OdbcDataAdapter
+        Dim dt As New DataTable
+        Dim str As String = "SELECT * FROM product WHERE productid=?"
+        Try
+            Call connectDB()
+            cmd = New Odbc.OdbcCommand(str, con)
+            cmd.Parameters.AddWithValue("@", id)
+            da.SelectCommand = cmd
+            da.Fill(dt)
+            txtID.Text = dt.Rows(0)(0).ToString
+            txtBrand.Text = dt.Rows(0)(1).ToString
+            txtDescription.Text = dt.Rows(0)(2).ToString
+            txtUnitPrice.Text = dt.Rows(0)(3).ToString
+        Catch ex As Exception
+            MsgBox(ex.Message.ToString)
+        Finally
+            da.Dispose()
+            dt.Dispose()
+            con.Close()
+            GC.Collect()
+        End Try
+    End Sub
+
+    Private Sub btnEdit_Click(sender As Object, e As EventArgs) Handles btnEdit.Click
+        If Val(dgv.Tag) = 0 Then
+            MsgBox("Please Select a Record to Edit!", vbCritical, "CRUD")
+        Else
+            If MsgBox("Are you sure you want to edit the selected record?", vbYesNo + vbQuestion, "EDIT") = vbNo Then
+                dgv.Tag = "0"
+            Else
+                Call cleaner(pnlDataEntry)
+                Call toggle(True)
+                Call loadRecord(Val(dgv.Tag))
+            End If
+        End If
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Dispose()
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Dim cmd As Odbc.OdbcCommand
+        Dim str As String = "DELETE FROM product where productid=?"
+        If Val(dgv.Tag) = 0 Then
+            MsgBox("Select a Record to Delete!", vbCritical, "DELETE")
+        Else
+            If MsgBox("Are you sure you want to DELETE the selected record?", vbYesNo + vbQuestion, "DELETE") = vbNo Then
+                dgv.Tag = "0"
+            Else
+                Try
+                    Call connectDB()
+                    cmd = New Odbc.OdbcCommand(str, con)
+                    cmd.Parameters.AddWithValue("@", Val(dgv.Tag))
+                    cmd.ExecuteNonQuery()
+                    Call loadDGV("SELECT * FROM product", dgv)
+                    MsgBox("Record was succesfully DELETED!", vbInformation, "DELETE")
+                    dgv.Tag = 0
+                Catch ex As Exception
+                    MsgBox(ex.Message.ToString)
+                Finally
+                    con.Close()
+                    GC.Collect()
+                End Try
+            End If
+        End If
+    End Sub
+
+    Private Sub txtUnitPrice_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtUnitPrice.KeyPress
+        Select Case Asc(e.KeyChar)
+            Case Asc("0") To Asc("9")
+            Case 8 ' backspace key
+            Case 13 ' enter key
+            Case Asc(".")
+                If InStr(txtUnitPrice.Text, ".", CompareMethod.Text) = 0 Then
+                    e.KeyChar = e.KeyChar
+                Else
+                    e.KeyChar = ""
+                End If
+            Case Else
+                e.KeyChar = ""
+        End Select
+    End Sub
+End Class
